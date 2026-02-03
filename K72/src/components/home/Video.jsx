01a6a4d2c85bf8d182a68c3da7ddafad192cd68c
Adarsh puri <152283128+Adarshpuri1@users.noreply.github.com>
2026-02-03 23:52:26 +0530

    Update Video.jsx

--- K72/src/components/home/Video.jsx
+++ K72/src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://drive.google.com/file/d/14SEq1P_PPYHo-Eslf716YqHkqneaBbQK/view?usp=sharing"></video>
     </div>
   )
 }
