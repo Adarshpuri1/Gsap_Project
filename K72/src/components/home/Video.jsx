b564f15803ebdfacf045c53ce0d6d3ae883682dd
Adarsh puri <152283128+Adarshpuri1@users.noreply.github.com>
2026-02-04 00:05:53 +0530

    Update Video.jsx

--- K72/src/components/home/Video.jsx
+++ K72/src/components/home/Video.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Video = () => {
+  const path="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1770143604~exp=1770147204~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=b2e9bd65d6b4bace827275874d1e69e439f77417db42ce81798caa96a948361e&r=dXMtZWFzdDE%3D";
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://drive.google.com/file/d/14SEq1P_PPYHo-Eslf716YqHkqneaBbQK/view?usp=sharing"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src={path}></video>
     </div>
   )
 }
